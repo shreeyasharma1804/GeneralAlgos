f61a04d1aef252998ae37bab7e9c6643f7d5119a
shreeya <shreeyasharmma@gmail.com>
2025-05-22 15:16:39 +0530

    dependency resolution using dfs

---
+++ dependency_resolution/main.py
@@ -0,0 +1,42 @@
+class dependency_resolution:
+
+    def __init__(self):
+        self.dependency_graph = {}
+        self.indegrees = {}
+
+    def add_dependency(self, b , a):
+        if(b in self.dependency_graph):
+            self.dependency_graph[b].append(a)
+        else:
+            self.dependency_graph[b] = [a]
+
+        if(a in self.indegrees):
+            self.indegrees[a] = self.indegrees[a]+1
+        else:
+            self.indegrees[a] = 1
+
+    def mock_install(self, node, visited):
+        if(node in visited):
+            return
+        if(node not in self.dependency_graph):
+            visited[node] = 1
+            return
+        for nei in self.dependency_graph[node]:
+            visited[node] = 1
+            self.mock_install(nei, visited)
+
+graph = dependency_resolution()
+graph.add_dependency("B", "A")
+graph.add_dependency("C", "A")
+graph.add_dependency("E", "B")
+graph.add_dependency("E", "C")
+graph.add_dependency("F", "C")
+graph.add_dependency("G", "B")
+graph.add_dependency("G", "D")
+graph.add_dependency("H", "F")
+graph.add_dependency("H", "E")
+graph.add_dependency("I", "G")
+
+all_dependencies = {}
+graph.mock_install('H', all_dependencies)
+print(all_dependencies)
